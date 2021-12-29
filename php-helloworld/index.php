59746cb3f215029fb4053d8d0f89f11ce52f276e
Student User <student@workstation.lab.example.com>
2021-12-29 10:01:53 -0500

    lpmb 6 10 lab

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,4 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "Hello, World! I am Patricio Moreno anf the php version is " . PHP_VERSION . "\n";
+print "Nice course \n"
 ?>
